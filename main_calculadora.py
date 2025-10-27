ac3fda1e79c1c59a0f5f873dac8f4420da2030ce
RCrislane <rcavalcanti.ads@gmail.com>
2025-10-27 19:49:50 -0300

    modificação(estrutura): interface tkinter a mostra - RAQUEL

---
+++ main_calculadora.py
@@ -0,0 +1,12 @@
+from tkinter import *   
+from tkinter import ttk       
+
+janela = Tk() 
+janela.title("Calculadora - titulo") # titulo da janela
+janela.geometry("235x326") # tamanho da janela
+janela.resizable(width= False, height= False) #ele restringe que vc tente abrir mais do que a tela existe
+janela.config(bg='black') # cor de fundo da janela
+
+
+
+janela.mainloop()
