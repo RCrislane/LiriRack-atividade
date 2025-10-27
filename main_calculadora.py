824081d5e649b03a2901533dec21bbce7c3c35be
RCrislane <rcavalcanti.ads@gmail.com>
2025-10-27 20:33:08 -0300

    modificações(frames): colocando telas de frames - raquel

--- main_calculadora.py
+++ main_calculadora.py
@@ -20,5 +20,16 @@ janela.geometry("235x326") # tamanho da janela
 janela.resizable(width= False, height= False) #ele restringe que vc tente abrir mais do que a tela existe
 janela.config(bg='black') # cor de fundo da janela
 
+#  bg: background - fundo fg: foreground
+# #criando frames
+frem_tela = Frame(janela, width=235, height=65, bg=bot_rosinha10)
+frem_tela.grid(row=0, column=0)                 #row=fileira, linha colunm= coluna #gri &  #color picker
+
+
+frem_corpo = Frame(janela, width=235, height=268, bg=bot_rosaclara9)
+frem_corpo.grid(row=1, column=0)                 #row=fileira, linha colunm= coluna #gri &  #color picker
+
+
+
 
 janela.mainloop()
