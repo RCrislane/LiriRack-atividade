76dceb7b073e5adbebac436905e453e42b32985a
Liriel Rosseli M. de Barros <lirielrosseli@gmail.com>
2025-10-27 19:55:57 -0300

    adicionando cores - liriel

--- main_calculadora.py
+++ main_calculadora.py
@@ -1,12 +1,23 @@
 from tkinter import *   
 from tkinter import ttk       
 
+#cores
+azul_escuro1 = "#010628"  #azul escuro
+branco2 = "#FEFFFE"  # branco
+laranja3 = "#FC4F0F" #laranja 
+azulzinho4 = "#61A5FF" #azulzinho fundo
+botao_laranja5 = "#FFAB40" #laranjain botões com os calculos
+bot_rosa6 = "#F603ED"
+bot_rosafundo = "#A957C0" # pa n°
+bot_rosaescuro8 = "#B0077D" # fundo
+bot_rosaclara9 = "#FBBBE7" #fundo
+bot_rosinha10 = "#E9B8DA"
+
 janela = Tk() 
 janela.title("Calculadora - titulo") # titulo da janela
 janela.geometry("235x326") # tamanho da janela
 janela.resizable(width= False, height= False) #ele restringe que vc tente abrir mais do que a tela existe
 janela.config(bg='black') # cor de fundo da janela
-
 
 
 janela.mainloop()
